0197cadc94912c7b0ac68f18928b29b278874a8e
Chris Dawson <xrdawson@gmail.com>
2014-01-03 22:43:57 -0800

    Initial checkin

---
+++ gen/R.java.d
@@ -0,0 +1,18 @@
+/Users/xrdawson/Projects/LeinDroid/clojuroid/gen/org/stuff/clojuroid/R.java \
+ : /Users/xrdawson/Projects/LeinDroid/clojuroid/res/anim/splash_rotation.xml \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/res/drawable/splash_background.xml \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/res/drawable-hdpi/ic_launcher.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/res/drawable-hdpi/splash_circle.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/res/drawable-hdpi/splash_droid.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/res/drawable-hdpi/splash_hands.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/res/drawable-ldpi/ic_launcher.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/res/drawable-mdpi/ic_launcher.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/res/layout/splashscreen.xml \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/res/values/strings.xml \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/target/res/drawable-hdpi/ic_launcher.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/target/res/drawable-hdpi/splash_circle.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/target/res/drawable-hdpi/splash_droid.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/target/res/drawable-hdpi/splash_hands.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/target/res/drawable-ldpi/ic_launcher.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/target/res/drawable-mdpi/ic_launcher.png \
+/Users/xrdawson/Projects/LeinDroid/clojuroid/AndroidManifest.xml \
