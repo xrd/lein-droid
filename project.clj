18e8d3b4e64e69e11800571f1cdf1a90b81232d3
Chris Dawson <xrdawson@gmail.com>
2014-01-09 16:32:21 -0800

    Updates based on @alexander-yakushev comments

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
   :java-source-paths ["src/java" "gen"]
 
   :dependencies [[org.clojure-android/clojure "1.5.1-jb" :use-resources true]
-                 [neko/neko "3.0.0-beta5"]]
+                 [neko/neko "3.0.0-preview3"]]
   :profiles {:dev {:dependencies [[android/tools.nrepl "0.2.0-bigstack"]
                                   [compliment "0.0.3"]]
                    :android {:aot :all-with-unused}}
